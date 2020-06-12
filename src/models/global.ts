4ddf0989f60c15bbba19dea18f8d2b52a41e65ea
SeekerGAO <1532584997@qq.com>
2020-06-12 18:19:02 +0800

    feat: Add models

--- src/models/global.ts
+++ src/models/global.ts
@@ -1,5 +1,7 @@
 import { Subscription, Reducer, Effect } from 'umi';
 
+import { querySystemParamConfigs, queryAllMerchants, queryAllBanks } from '@/services/global';
+import { GETSYSTEMPARAMCONFIGS, GETALLMERCHANTS, GETALLBANKS } from '@/constants/global';
 import { NoticeIconData } from '@/components/NoticeIcon';
 import { queryNotices } from '@/services/user';
 import { ConnectState } from './connect.d';
@@ -10,7 +12,19 @@ export interface NoticeItem extends NoticeIconData {
   status: string;
 }
 
+export interface SystemParamConfigs {
+  thirdParty: object[];
+  paymentChannel: object[];
+  licensePlateWay: object[];
+  devices: object[];
+  oils: object[];
+  businessType: object[];
+}
+
 export interface GlobalModelState {
+  systemParamConfigs: SystemParamConfigs;
+  merchants: object[];
+  banks: object[];
   collapsed: boolean;
   notices: NoticeItem[];
 }
@@ -19,14 +33,20 @@ export interface GlobalModelType {
   namespace: 'global';
   state: GlobalModelState;
   effects: {
+    fetchSystemParamConfigs: Effect;
+    fetchMerchants: Effect;
+    fetchBanks: Effect;
     fetchNotices: Effect;
     clearNotices: Effect;
     changeNoticeReadState: Effect;
   };
   reducers: {
-    changeLayoutCollapsed: Reducer<GlobalModelState>;
-    saveNotices: Reducer<GlobalModelState>;
-    saveClearedNotices: Reducer<GlobalModelState>;
+    GETSYSTEMPARAMCONFIGS: Reducer;
+    GETALLMERCHANTS: Reducer;
+    GETALLBANKS: Reducer;
+    changeLayoutCollapsed: Reducer;
+    saveNotices: Reducer;
+    saveClearedNotices: Reducer;
   };
   subscriptions: { setup: Subscription };
 }
@@ -35,11 +55,65 @@ const GlobalModel: GlobalModelType = {
   namespace: 'global',
 
   state: {
+    systemParamConfigs: {
+      thirdParty: [],
+      paymentChannel: [],
+      licensePlateWay: [],
+      devices: [],
+      oils: [],
+      businessType: [],
+    },
+    merchants: [],
+    banks: [],
     collapsed: false,
     notices: [],
   },
 
   effects: {
+    *fetchSystemParamConfigs({ payload }, { call, put }) {
+      const response = yield call(querySystemParamConfigs, payload);
+
+      const { type } = payload;
+      const systemParamConfigs: SystemParamConfigs = {};
+
+      if (type === 0) {
+        systemParamConfigs.thirdParty = response;
+      }
+      if (type === 1) {
+        systemParamConfigs.paymentChannel = response;
+      }
+      if (type === 2) {
+        systemParamConfigs.licensePlateWay = response;
+      }
+      if (type === 3) {
+        systemParamConfigs.devices = response;
+      }
+      if (type === 4) {
+        systemParamConfigs.oils = response;
+      }
+      if (type === 6) {
+        systemParamConfigs.businessType = response;
+      }
+
+      yield put({
+        type: GETSYSTEMPARAMCONFIGS,
+        payload: response,
+      });
+    },
+    *fetchMerchants({ payload }, { call, put }) {
+      const response = yield call(queryAllMerchants, payload);
+      yield put({
+        type: GETALLMERCHANTS,
+        payload: response,
+      });
+    },
+    *fetchBanks(_, { call, put }) {
+      const response = yield call(queryAllBanks);
+      yield put({
+        type: GETALLBANKS,
+        payload: response,
+      });
+    },
     *fetchNotices(_, { call, put, select }) {
       const data = yield call(queryNotices);
       yield put({
@@ -101,6 +175,24 @@ const GlobalModel: GlobalModelType = {
   },
 
   reducers: {
+    GETSYSTEMPARAMCONFIGS(state, { payload }) {
+      return {
+        ...state,
+        systemParamConfigs: payload,
+      };
+    },
+    GETALLMERCHANTS(state, { payload }) {
+      return {
+        ...state,
+        merchants: payload,
+      };
+    },
+    GETALLBANKS(state, { payload }) {
+      return {
+        ...state,
+        banks: payload,
+      };
+    },
     changeLayoutCollapsed(state = { notices: [], collapsed: true }, { payload }): GlobalModelState {
       return {
         ...state,
